65157378138ca214eb0472bddaae1b565a288d02
mastaryan <sizemore.ryan@gmail.com>
2020-01-20 14:40:54 -0500

    Updates to contact form

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: BluegrassDoodles'; 
+    $to = 'wsizemore00@gmail.com'; 
+    $subject = 'Hello from Bluegrass Doodles';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+    if ($_POST['submit'] && $human == '4') {
+        if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Your message has been sent!</p>';
+    } else { 
+        echo '<p>Something went wrong, go back and try again!</p>'; 
+    }
+} else if ($_POST['submit'] && $human != '4') {
+    echo '<p>You answered the anti-spam question incorrectly!</p>';
+}
+    
+?>
