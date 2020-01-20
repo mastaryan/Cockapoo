31a853e26da97d54a7eed99a3d0f0119e1966351
mastaryan <sizemore.ryan@gmail.com>
2020-01-20 15:04:37 -0500

    fix php again

--- index.php
+++ index.php
@@ -2,19 +2,20 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: BluegrassDoodles'; 
+    $from = 'From: Bluegrass Doodles'; 
     $to = 'wsizemore00@gmail.com'; 
     $subject = 'Hello from Bluegrass Doodles';
-
+    $human = $_POST['human'];
+			
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-    if ($_POST['submit'] && $human == '4') {
+				
+    if ($_POST['submit'] && $human == '4') {				 
         if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Your message has been sent!</p>';
-    } else { 
-        echo '<p>Something went wrong, go back and try again!</p>'; 
+	    echo '<p>Your message has been sent!</p>';
+	} else { 
+	    echo '<p>Something went wrong, go back and try again!</p>'; 
+	} 
+    } else if ($_POST['submit'] && $human != '4') {
+	echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
-} else if ($_POST['submit'] && $human != '4') {
-    echo '<p>You answered the anti-spam question incorrectly!</p>';
-}
-    
 ?>
