91ee49c0e103109191c911f10bfa62e7c600f7f5
mastaryan <sizemore.ryan@gmail.com>
2020-03-16 23:51:03 -0400

    mailphp2

--- mail.php
+++ mail.php
@@ -1,37 +1,11 @@
-
 <?php 
-if(isset($_POST['submit'])){
-    $to = "wsizemore00@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-
-    // ANTI SPAM	
-    // if ($_POST['submit'] && $human == '4') {				 
-    //     if (mail ($to, $subject, $body, $from)) { 
-	//     echo '<p>Your message has been sent!</p>';
-	// } else { 
-	//     echo '<p>Something went wrong, go back and try again!</p>'; 
-	// } 
-    // } else if ($_POST['submit'] && $human != '4') {
-	// echo '<p>You answered the anti-spam question incorrectly!</p>';
-    // }
-    // END ANTI SPAM
-
-    ini_set('display_startup_errors', 1);
-    ini_set('display_errors', 1);
-    error_reporting(-1);
-
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "wsizemore00@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
