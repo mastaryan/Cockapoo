6505285265db6aafc42cad1cbacd00fc5f05e64c
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-09-03 18:49:54 -0400

    updated contact email address to puppies@bluegrassdoodle.com

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "wsizemore00@gmail.com";
+$recipient = "puppies@bluegrassdoodle.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
